38bc6bc5353391a7e241104234c353c2d61de822
kbn4191 <kaluboniface@yahoo.com>
2025-05-19 09:09:54 +0100

    search suspense boundry

--- src/app/(page)/search/page.tsx
+++ src/app/(page)/search/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function SearchPage() {
+function SearchResult() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
 
   return <div>Search results for: {query}</div>;
+}
+
+export default function SearchPage() {
+  return (
+    <Suspense fallback={<p>Loading search...</p>}>
+      <SearchResult />
+    </Suspense>
+  );
 }
